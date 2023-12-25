cd7124f36f430fc8fb2983e77c8981e2235c495d
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-12-24 19:14:59 -0500

    01_10-capitalize-challenge

--- javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
+++ javascript-sandbox-start/01-variables-data-types/10-capitalize-challenge/script.js
@@ -1,2 +1,14 @@
 // Create a new string called "myNewString" that holds the value of "Developer", using the value from "myString"
-const myString = 'developer';
+const myString = 'developer and dreamer';
+
+function capitalize(str) {
+    const words = str.split(' ');
+    for (let i = 0; i < words.length; i++) {
+        const capLetter = words[i][0].toUpperCase();
+        words[i] = capLetter + words[i].slice(1);        
+    }
+    const myNewString = words.join(' ');
+    return myNewString;
+}
+
+console.log(capitalize(myString));
