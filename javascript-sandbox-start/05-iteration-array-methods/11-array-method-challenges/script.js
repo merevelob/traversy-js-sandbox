c5083f3a74a0f8229ed4a699924e60c43e293274
Mauricio Revelo <122694874+merevelob@users.noreply.github.com>
2023-12-26 19:53:14 -0500

    05_11-array-method-challenges

--- javascript-sandbox-start/05-iteration-array-methods/11-array-method-challenges/script.js
+++ javascript-sandbox-start/05-iteration-array-methods/11-array-method-challenges/script.js
@@ -37,5 +37,38 @@ const people = [
   },
 ];
 
+const youngPeople = people
+  .filter(person => person.age <= 25)
+  .map(person => ({
+    name: `${person.firstName} ${person.lastName}`,
+    email: person.email,
+  }));
+
+console.log(youngPeople);
+
+
 // Challenge 2
 const numbers = [2, -30, 50, 20, -12, -9, 7];
+
+const positiveSum = numbers
+  .filter(num => num > 0)
+  .reduce((x0, x1) => x0 + x1);
+
+console.log(positiveSum);
+
+// Challenge 3
+function capitalize(str) {
+  const words = str.split(' ');
+  for (let i = 0; i < words.length; i++) {
+      const capLetter = words[i][0].toUpperCase();
+      words[i] = capLetter + words[i].slice(1);        
+  }
+  const myNewString = words.join(' ');
+  return myNewString;
+}
+
+const words = ['coder', 'programmer', 'developer'];
+
+const capitalizedWords = words.map(capitalize);
+
+console.log(capitalizedWords);
